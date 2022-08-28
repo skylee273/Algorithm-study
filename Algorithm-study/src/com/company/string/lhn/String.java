102ab0a69de27d4400a1651024d859a90550545b
skylee273 <skylee273@koreatech.ac.kr>
2022-08-28 22:18:39 +0900

    feat(String Code) : character search

---
+++ Algorithm-study/src/com/company/string/lhn/String.java
@@ -0,0 +1,12 @@
+package com.company.string.lhn;
+
+public class String {
+
+    public int solution1(java.lang.String str, char c){
+        int solution = 0;
+        for (char find : str.toCharArray()) {
+            if(find == c) solution++;
+        }
+        return solution;
+    }
+}
