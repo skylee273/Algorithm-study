8287d1077a82aac73f19765e96ac34c9c0fb4bb1
skylee273 <skylee273@koreatech.ac.kr>
2022-08-28 22:30:32 +0900

    feat(string branch) : string branch add

--- Algorithm-study/src/com/company/string/lhn/String.java
+++ Algorithm-study/src/com/company/string/lhn/String.java
@@ -2,7 +2,7 @@ package com.company.string.lhn;
 
 public class String {
 
-    //dev-master
+    //string branch
     public int solution1(java.lang.String str, char c){
         int solution = 0;
         for (char find : str.toCharArray()) {
